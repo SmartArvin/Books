5813f59d44394efde7c01575e54d04e079a583fa
SmartArvin <smartarvin199111@gmail.com>
2018-09-26 12:33:37 +0800

    删除报病毒bat

--- 原创/bat批处理系列/批处理实例/精确延时工具/autoup.vbs
+++
@@ -1,4 +0,0 @@
-on error resume next 
-set shell=createobject("wscript.shell") 
-wscript.sleep 1000
-shell.run "auto.bat /start",0 
--- 原创/bat批处理系列/批处理实例/网络类/自动改回主页.bat
+++
@@ -1,3 +0,0 @@
-reg add "HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\Main" /v "Start Page" /t reg_sz /d http://www.21cn.com /f 
-reg add "HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\Main" /v "Default_Page_URL" /t reg_sz /d http://www.21cn.com /f 
-pause
